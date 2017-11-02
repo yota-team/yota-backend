8fde3555ec0c749408e79b9dfe884fff82e57851
irianto <irianto223@gmail.com>
2017-11-02 22:02:10 +0700

    backend dengan data dummy done

--- models/Car.js
+++ models/Car.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const carSchema = new Schema ({
   car_code: String,
-  destinations: [{type: Schema.Types.ObjectId, ref: 'Destination'}]
+  destination: {type: Schema.Types.ObjectId, ref: 'Destination'}
 },{
   timestamps: true
 })
